c39f968ac7c2b4dc50e2a40cc54da25fce7bba9d
João Victor C Baptista <76636099+joaov30@users.noreply.github.com>
2022-10-08 17:13:05 -0300

    Update Estudando SQL.sql

--- Estudando SQL.sql
+++ Estudando SQL.sql
@@ -27,18 +27,17 @@ WHERE FirstName LIKE '%ke%';
 */
 
 
+
 --Desafio 1
 SELECT *
 FROM Production.Product
 WHERE Weight > 500 AND Weight < 700;
 -------------------------------------------
-
 --Desafio 2
 SELECT *
 FROM HumanResources.Employee
 WHERE MaritalStatus = 'M' AND SalariedFlag = 1;
 -------------------------------------------
-
 --Desafio 3
 SELECT p.BusinessEntityID, p.FirstName, p.LastName, pe.BusinessEntityID, pe.EmailAddress
 FROM Person.Person AS p
@@ -46,84 +45,64 @@ LEFT JOIN Person.EmailAddress as pe
 on p.BusinessEntityID = pe.BusinessEntityID
 WHERE FirstName = 'Peter' AND LastName = 'Krebs';
 -------------------------------------------
-
 --Desafio 4
 SELECT COUNT(DISTINCT p.FirstName) as PrimeiroNome
 FROM Person.Person as p
 -------------------------------------------
-
 --Desafio 5
 SELECT COUNT(*)
 FROM Production.Product
 -------------------------------------------
-
 --Desafio 6
 SELECT COUNT(Size)
 FROM Production.Product
 -------------------------------------------
-
 --Desafio 7
 SELECT COUNT(DISTINCT p.Size) as Tamanho
 FROM Production.Product as p
 -------------------------------------------
-
 --Desafio 8
 SELECT TOP (10) *
 FROM Production.Product
-
-
 -------------------------------------------
-
 --Desafio 9
 SELECT TOP (10) P.ProductID, p.ListPrice
 FROM Production.Product as p
 ORDER BY  ListPrice DESC
-
 -------------------------------------------
-
 --Desafio 10
 SELECT TOP (4) p.Name, p.ProductNumber
 FROM Production.Product as p
 ORDER BY ProductID ASC;
-
 -------------------------------------------
-
 --Desafio 11
 SELECT COUNT(p.ListPrice) as listaPreco
 FROM Production.Product as p
 WHERE ListPrice > '1500'
-
 -------------------------------------------
 --Desafio 12
 SELECT COUNT(p.LastName) as LastName
 FROM Person.Person as p
 WHERE LastName LIKE 'P%'
-
 -------------------------------------------
-
 --Desafio 13
 SELECT COUNT(DISTINCT a.City) AS City
 FROM Person.Address AS a
-
 -------------------------------------------
-
 --Desafio 14
 SELECT DISTINCT a.City AS City
 FROM Person.Address AS a
-
 -------------------------------------------
-
 --Desafio 15
 SELECT COUNT(Color) as color
 FROM Production.Product 
 WHERE Color LIKE 'Red' 
 AND ListPrice BETWEEN '500' AND '1000';
-
 -------------------------------------------
-
 --Desafio 16
-
-
+SELECT COUNT(p.Name) AS Name
+FROM Production.Product as p
+WHERE Name LIKE '%road%';
 -------------------------------------------
 
 
