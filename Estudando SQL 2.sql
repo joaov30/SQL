a0af1ed4c2a37e8d445879d10fd59811ff6d23c8
João Victor C Baptista <76636099+joaov30@users.noreply.github.com>
2022-10-14 19:36:09 -0300

    Create Estudando SQL 2.sql

---
+++ Estudando SQL 2.sql
@@ -0,0 +1,39 @@
+--SELECT NOME_COLUNA
+--FROM TABELA A, TABELA B
+--WHERE CONDICAO
+
+
+--SELFJOIN
+--Desafios 28
+SELECT *
+FROM Customers
+
+SELECT a.ContactName, B.Region
+FROM Customers A, Customers B
+WHERE A.Region = b.Region
+
+--------
+
+SELECT *
+FROM Employees
+
+SELECT A.firstName, A.HireDate, B.firstName, B.HireDate
+FROM Employees A, Employees B
+Where DATEPART(YEAR, A.hiredate) = DATEPART(YEAR, B.hiredate)
+
+
+--------
+
+SELECT *
+FROM [Order Details]
+
+SELECT A.productID, A.Discount, B.productID, B.Discount
+FROM [Order Details] A, [Order Details] B
+WHERE A.Discount = B.Discount
+--======================================================================================================
+
+--Desafio 29
+
+
+
+-------------------------------------------
